8cb12922f908ce0e049f2b652cce1bae7f22e91a
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-20 11:26:52 -0300

    Department class created

--- SalesMVC/Controllers/HomeController.cs
+++ SalesMVC/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesMVC.Models;
+using SalesMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
