d30e503dc6ea70a5103262da507ac2856124f1b4
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-22 18:53:53 -0300

    Simple Seller form created"

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -1,25 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesMVC.Models;
 using SalesMVC.Services;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SalesMVC.Controllers {
-    
+
     public class SellersController : Controller {
-        
+
         private readonly SellerService _service;
 
         public SellersController(SellerService service) {
             _service = service;
         }
-        
+
         public IActionResult Index() {
             List<Seller> list = _service.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _service.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- SalesMVC/Services/SellerService.cs
+++ SalesMVC/Services/SellerService.cs
@@ -17,5 +17,10 @@ namespace SalesMVC.Services {
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller) {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
