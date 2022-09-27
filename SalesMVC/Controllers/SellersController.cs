34af07b4628a9d6349b3d30b5f40e88c0154272c
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-27 11:27:24 -0300

    Custom error page

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -2,8 +2,9 @@
 using SalesMVC.Models;
 using SalesMVC.Models.ViewModels;
 using SalesMVC.Services;
-using SalesMVC.Services.Exceptions;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SalesMVC.Controllers {
 
@@ -36,9 +37,9 @@ namespace SalesMVC.Controllers {
         }
 
         public IActionResult Delete(int? id) {
-            if (id == null) return NotFound();
+            if (id == null) return RedirectToAction(nameof(Error), new { message = "Id não fornecido!" });
             Seller seller = _service.FindById(id.Value);
-            if (seller == null) return NotFound();  
+            if (seller == null) return RedirectToAction(nameof(Error), new { message = "Id não encontrado!" });
             return View(seller);
         }
 
@@ -50,16 +51,16 @@ namespace SalesMVC.Controllers {
         }
 
         public IActionResult Details(int? id) {
-            if (id == null) return NotFound();
+            if (id == null) return RedirectToAction(nameof(Error), new { message = "Id não fornecido!" });
             Seller seller = _service.FindById(id.Value);
-            if (seller == null) return NotFound();
+            if (seller == null) return RedirectToAction(nameof(Error), new { message = "Id não encontrado!" });
             return View(seller);
         }
 
         public IActionResult Edit(int? id) {
-            if (id == null) return NotFound();
+            if (id == null) return RedirectToAction(nameof(Error), new { message = "Id não fornecido!" });
             Seller seller = _service.FindById(id.Value);
-            if (seller == null) return NotFound();
+            if (seller == null) return RedirectToAction(nameof(Error), new { message = "Id não encontrado!" });
             List<Department> departments = _depService.FindAll();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
             return View(viewModel);
@@ -68,15 +69,21 @@ namespace SalesMVC.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) {
-            if (id != seller.Id) return BadRequest();
+            if (id != seller.Id) return RedirectToAction(nameof(Error), new { message = "Ids não correspondem!" });
             try {
                 _service.Update(seller);
                 return RedirectToAction(nameof(Index));
-            } catch (NotFoundException) {
-                return NotFound();
-            } catch (DBConcurrencyException) {
-                return BadRequest();
+            } catch (ApplicationException e) {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
+        }
+
+        public IActionResult Error(string message) {
+            ErrorViewModel viewModel = new ErrorViewModel {
+                Mesage = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
--- SalesMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesMVC/Models/ViewModels/ErrorViewModel.cs
@@ -3,6 +3,7 @@ using System;
 namespace SalesMVC.Models.ViewModels {
     public class ErrorViewModel {
         public string RequestId { get; set; }
+        public string Mesage { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
